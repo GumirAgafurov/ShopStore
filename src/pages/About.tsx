8bb15d0e1202dfcc56d50c2396856581d9560508
GumirYo <agafurov.gumir@gmail.com>
2026-02-02 16:32:26 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,11 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export function About() {
-    return <h1>About</h1>
+    return <div>
+      <h1>About</h1>
+      <Link to="/store">
+        <Button>Перейти в магазин</Button>
+      </Link>
+    </div>
 }
