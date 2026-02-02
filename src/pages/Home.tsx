8797d83596adce416f752e51cda3a367427f6aca
GumirYo <agafurov.gumir@gmail.com>
2026-02-02 16:32:39 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,11 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export function Home() {
-    return <h1>Home</h1>
+    return <div>
+      <h1>Home</h1>
+      <Link to="/store">
+        <Button>Перейти в магазин</Button>
+      </Link>
+    </div>
 }
